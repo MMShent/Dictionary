9919e7997aae5c0fab0cd2d9c84d3f27cba6a36f
Ivan Shentov <ivan.shentov@mentormate.com>
2011-07-11 11:15:38 +0300

    little fixes

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -4,12 +4,13 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
-//if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '46.249.76.133', '::1')))
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'dev1.mentormate.com', 'dev2.mentormate.com', '78.90.177.7', '::1')))
+/*
+if (!in_array(@$_SERVER['REMOTE_ADDR'], 
+   array('127.0.0.1', 'dev1.mentormate.com', 'dev2.mentormate.com', '78.90.177.7', '46.249.76.133', '::1')))
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
